79fa99734e40ebe36f930285228cdf04fc66400e
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-03 11:02:44 +0200

    Redesign interface and integrate new logo v1.1.0

--- app/src/main/java/com/chasmet/remplissagepapierofficiel/MainActivity.java
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
     @Override
@@ -12,10 +13,14 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         Button btnNewDocument = findViewById(R.id.btnNewDocument);
+        Button btnImportPdf = findViewById(R.id.btnImportPdf);
+        Button btnDocuments = findViewById(R.id.btnDocuments);
         Button btnProfile = findViewById(R.id.btnProfile);
         Button btnSettings = findViewById(R.id.btnSettings);
 
         btnNewDocument.setOnClickListener(v -> startActivity(new Intent(this, EditorActivity.class)));
+        btnImportPdf.setOnClickListener(v -> startActivity(new Intent(this, EditorActivity.class)));
+        btnDocuments.setOnClickListener(v -> Toast.makeText(this, "Mes documents sera enrichi dans la prochaine amélioration.", Toast.LENGTH_SHORT).show());
         btnProfile.setOnClickListener(v -> startActivity(new Intent(this, ProfileActivity.class)));
         btnSettings.setOnClickListener(v -> startActivity(new Intent(this, SettingsActivity.class)));
     }
