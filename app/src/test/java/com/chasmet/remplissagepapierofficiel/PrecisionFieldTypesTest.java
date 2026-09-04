25f16a4442b19ec0854c21bee1bd206fe5a1c0f3
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:37:16 +0200

    Test identity field hint type

---
+++ app/src/test/java/com/chasmet/remplissagepapierofficiel/PrecisionFieldTypesTest.java
@@ -0,0 +1,4 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class PrecisionFieldTypesTest { @Test public void identityType(){assertEquals("identity",PrecisionFieldTypes.IDENTITY);} }
