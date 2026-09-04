08ffffe730822e6123da8325983afa6f3c044c65
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:25:10 +0200

    Test exact correction delta

---
+++ app/src/test/java/com/chasmet/remplissagepapierofficiel/OverlayMutationTest.java
@@ -0,0 +1,4 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class OverlayMutationTest { @Test public void exactDelta(){ TextOverlay a=new TextOverlay("x",0,.5f,.5f,"x",8f,"left","text",0,0,"known"); TextOverlay b=a.withPosition(.5f,.496f); assertEquals(-.004f,new OverlayMutation(a,b).yDelta(),.000001f); } }
