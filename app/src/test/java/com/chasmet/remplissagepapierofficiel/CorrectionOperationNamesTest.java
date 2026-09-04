c6fda2d9aa49446aea2f4aae41264a8c2c405679
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:28:33 +0200

    Test correction operation names

---
+++ app/src/test/java/com/chasmet/remplissagepapierofficiel/CorrectionOperationNamesTest.java
@@ -0,0 +1,4 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class CorrectionOperationNamesTest { @Test public void updateStable(){assertEquals("update_overlay",CorrectionOperationNames.UPDATE);} }
