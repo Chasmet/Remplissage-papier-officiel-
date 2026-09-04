b3662cad735f764c6f0db0e6f0b8d4cbcf57253b
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:35:44 +0200

    Add correction performance policy

---
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/PrecisionPerformance.java
@@ -0,0 +1,2 @@
+package com.chasmet.remplissagepapierofficiel;
+public final class PrecisionPerformance { private PrecisionPerformance(){} public static final boolean REBUILD_ALL_OVERLAYS_ON_LOCAL_UPDATE=false; public static final boolean RERENDER_CHANGED_PAGES=true; public static final boolean SEND_CROPS_ONLY_FOR_MODIFIED_OVERLAYS=true; }
