a646110c9f2367234a4725ba94efbcec9fccd871
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:37:27 +0200

    Add correction authority policy

---
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/PrecisionCorrectionPolicy.java
@@ -0,0 +1,2 @@
+package com.chasmet.remplissagepapierofficiel;
+public final class PrecisionCorrectionPolicy { private PrecisionCorrectionPolicy(){} public static final boolean CHATGPT_DECIDES_POSITION=true;public static final boolean ANDROID_APPLIES_EXACTLY=true;public static final boolean ANDROID_MAY_SUGGEST_WARNINGS=true;public static final boolean ANDROID_MAY_CHANGE_COORDINATES=false; }
