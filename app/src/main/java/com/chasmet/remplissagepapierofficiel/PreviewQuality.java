af14703d9860d1245e727a98a8961ba76e8f9798
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:26:31 +0200

    Define high resolution preview policy

---
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/PreviewQuality.java
@@ -0,0 +1,2 @@
+package com.chasmet.remplissagepapierofficiel;
+public final class PreviewQuality { private PreviewQuality(){} public static final int FULL_PAGE_MAX_DIMENSION=2000; public static final int CROP_MAX_DIMENSION=1200; public static final int JPEG_QUALITY=90; }
