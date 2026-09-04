d78761e57168c61337d831fefd462fb3699cd0b4
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:40:44 +0200

    Test precision vectors present

---
+++ app/src/test/java/com/chasmet/remplissagepapierofficiel/PrecisionTestVectorsTest.java
@@ -0,0 +1,4 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class PrecisionTestVectorsTest { @Test public void vectors(){assertEquals(3,PrecisionTestVectors.json().length());} }
