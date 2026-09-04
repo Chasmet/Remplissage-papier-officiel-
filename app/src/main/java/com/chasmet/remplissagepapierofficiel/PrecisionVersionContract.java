fc9c4cbe3a916f8b33019f46b63fef58334dfd2d
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:44:44 +0200

    Add app/server precision version contract

---
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/PrecisionVersionContract.java
@@ -0,0 +1,3 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.json.JSONObject;
+public final class PrecisionVersionContract { private PrecisionVersionContract(){} public static JSONObject json() throws Exception {return new JSONObject().put("android","1.10.0").put("android_version_code",26).put("mcp_minimum","4.1.0").put("protocol","1.0");} }
