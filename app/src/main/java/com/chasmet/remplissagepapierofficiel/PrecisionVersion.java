da062ec3f1c5c09b6c9f7b660d3423cba6bc2d6d
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:24:56 +0200

    Add precision engine version metadata

---
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/PrecisionVersion.java
@@ -0,0 +1,2 @@
+package com.chasmet.remplissagepapierofficiel;
+public final class PrecisionVersion { private PrecisionVersion(){} public static final String ENGINE="1.10.0"; public static final int PROTOCOL=1; }
