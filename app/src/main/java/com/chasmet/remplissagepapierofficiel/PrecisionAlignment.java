abf117ec424144bd71997713a3a2c8a9975c3f1e
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:38:28 +0200

    Add supported alignment descriptor

---
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/PrecisionAlignment.java
@@ -0,0 +1,3 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.json.JSONArray;
+public final class PrecisionAlignment { private PrecisionAlignment(){} public static JSONArray json(){return new JSONArray().put("left").put("center").put("right");} }
