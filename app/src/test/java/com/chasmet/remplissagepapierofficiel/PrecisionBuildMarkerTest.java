6bfe79169ef9bd9e8b6bfafe70e243ab13aed60b
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:29:42 +0200

    Test build marker version

---
+++ app/src/test/java/com/chasmet/remplissagepapierofficiel/PrecisionBuildMarkerTest.java
@@ -0,0 +1,4 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class PrecisionBuildMarkerTest { @Test public void versionMatches(){assertEquals("1.10.0",PrecisionBuildMarker.VERSION);} }
