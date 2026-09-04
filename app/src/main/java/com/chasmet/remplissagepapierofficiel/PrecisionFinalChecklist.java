fc485eee95b1b6c0395f72c6ababf96b9ebf3e15
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:43:03 +0200

    Add final pre-merge checklist

---
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/PrecisionFinalChecklist.java
@@ -0,0 +1,3 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.json.JSONObject;
+public final class PrecisionFinalChecklist { private PrecisionFinalChecklist(){} public static JSONObject json() throws Exception {return new JSONObject().put("android_version","1.10.0").put("version_code",26).put("java",17).put("min_sdk",21).put("target_sdk",34).put("compile_sdk",34).put("tests_required",true).put("lint_required",true).put("assemble_debug_required",true).put("signed_release_required",true).put("mcp_4_1_required",true);} }
