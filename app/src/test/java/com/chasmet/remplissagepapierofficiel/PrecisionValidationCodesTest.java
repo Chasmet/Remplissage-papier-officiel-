e41050c538d088b8df06ab00d6779bb31b018744
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:34:08 +0200

    Test validation correction code

---
+++ app/src/test/java/com/chasmet/remplissagepapierofficiel/PrecisionValidationCodesTest.java
@@ -0,0 +1,4 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class PrecisionValidationCodesTest { @Test public void correctionCodeStable(){assertEquals("needs_correction",PrecisionValidationCodes.NEEDS_CORRECTION);} }
