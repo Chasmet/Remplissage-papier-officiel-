d676e367ff7af4b4426c80d635f832c4bd71ee72
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:36:47 +0200

    Add final export geometry policy

---
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/PrecisionExportPolicy.java
@@ -0,0 +1,2 @@
+package com.chasmet.remplissagepapierofficiel;
+public final class PrecisionExportPolicy { private PrecisionExportPolicy(){} public static final boolean PRESERVE_PAGE_SIZE=true;public static final boolean PRESERVE_PAGE_COUNT=true;public static final boolean USE_AUTHORITATIVE_OVERLAY_COORDINATES=true;public static final boolean APPLY_AUTO_SNAP=false; }
