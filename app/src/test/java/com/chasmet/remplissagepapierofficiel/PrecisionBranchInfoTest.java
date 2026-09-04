91d7eb251d65bdf8565670b8ffcde00d373787c1
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:43:54 +0200

    Test precision target branch

---
+++ app/src/test/java/com/chasmet/remplissagepapierofficiel/PrecisionBranchInfoTest.java
@@ -0,0 +1,3 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.junit.Test;import static org.junit.Assert.*;
+public class PrecisionBranchInfoTest { @Test public void targetMain(){assertEquals("main",PrecisionBranchInfo.TARGET);} }
