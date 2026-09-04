57dd1130fcd802cffbff7f68b8de1d66253a9ae2
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:31:08 +0200

    Test protocol version

---
+++ app/src/test/java/com/chasmet/remplissagepapierofficiel/PrecisionProtocolVersionTest.java
@@ -0,0 +1,4 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class PrecisionProtocolVersionTest { @Test public void protocolOne(){assertEquals("1.0",PrecisionProtocolVersion.value());} }
