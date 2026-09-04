f0b8b259e4d33918aaf95ba5ce84ff51843b3bde
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:42:41 +0200

    Test blocking severity constant

---
+++ app/src/test/java/com/chasmet/remplissagepapierofficiel/PrecisionQualityLevelsTest.java
@@ -0,0 +1,3 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.junit.Test;import static org.junit.Assert.*;
+public class PrecisionQualityLevelsTest { @Test public void blockingStable(){assertEquals("blocking",PrecisionQualityLevels.BLOCKING);} }
