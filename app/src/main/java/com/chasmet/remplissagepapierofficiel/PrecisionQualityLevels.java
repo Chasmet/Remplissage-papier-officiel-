d7e75973db19629b88094e8bb10f654b8d633cb3
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:42:36 +0200

    Add quality severity constants

---
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/PrecisionQualityLevels.java
@@ -0,0 +1,2 @@
+package com.chasmet.remplissagepapierofficiel;
+public final class PrecisionQualityLevels { private PrecisionQualityLevels(){} public static final String INFO="info";public static final String WARNING="warning";public static final String BLOCKING="blocking"; }
