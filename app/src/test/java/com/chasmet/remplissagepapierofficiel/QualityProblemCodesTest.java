4a2ddbe4c8d7a8e792003f0fccf3c32301f6fe09
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:26:18 +0200

    Test stable quality warning codes

---
+++ app/src/test/java/com/chasmet/remplissagepapierofficiel/QualityProblemCodesTest.java
@@ -0,0 +1,4 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class QualityProblemCodesTest { @Test public void touchLineCodeStable(){assertEquals("text_touches_line",QualityProblemCodes.TEXT_TOUCHES_LINE);} }
