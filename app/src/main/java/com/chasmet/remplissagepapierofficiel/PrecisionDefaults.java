c14646b3b498e35e425bfbd6de429dc86fb39dda
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:31:20 +0200

    Add conservative precision defaults

---
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/PrecisionDefaults.java
@@ -0,0 +1,2 @@
+package com.chasmet.remplissagepapierofficiel;
+public final class PrecisionDefaults { private PrecisionDefaults(){} public static final float DEFAULT_TEXT_SIZE=8f; public static final float CROP_MARGIN_X=.025f; public static final float CROP_MARGIN_Y=.015f; public static final String DEFAULT_ALIGN="left"; }
