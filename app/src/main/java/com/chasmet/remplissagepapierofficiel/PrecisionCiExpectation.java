5c3556d4805d121c1644289f18999b2ba3eb208a
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:43:42 +0200

    Add CI expectation metadata

---
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/PrecisionCiExpectation.java
@@ -0,0 +1,3 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.json.JSONArray;
+public final class PrecisionCiExpectation { private PrecisionCiExpectation(){} public static JSONArray json(){return new JSONArray().put("testDebugUnitTest").put("lintDebug").put("assembleDebug").put("assembleRelease").put("verify permanent signer").put("publish signed release");} }
