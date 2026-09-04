a445273abca3609b2f4400e75ac2439dd3a2ea4d
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:32:24 +0200

    Add shared precision constants

---
+++ app/src/main/java/com/chasmet/remplissagepapierofficiel/PrecisionConstants.java
@@ -0,0 +1,2 @@
+package com.chasmet.remplissagepapierofficiel;
+public final class PrecisionConstants { private PrecisionConstants(){} public static final String COORDINATE_SYSTEM="normalized-0-to-1"; public static final String ORIGIN="top_left"; public static final String TEXT_Y="baseline"; public static final String CHECKBOX_XY="center"; }
