c20cad603dd4b71b1e53e4100cff8028b958a15b
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:34:23 +0200

    Test correction phase name

---
+++ app/src/test/java/com/chasmet/remplissagepapierofficiel/PrecisionLoopPhasesTest.java
@@ -0,0 +1,4 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class PrecisionLoopPhasesTest { @Test public void correctionPhaseStable(){assertEquals("correct",PrecisionLoopPhases.CORRECT);} }
