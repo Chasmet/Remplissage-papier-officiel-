2a43b04960fa2aa494e96a2b0d1a8ed768cdd0d4
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-09-04 20:40:01 +0200

    Test paper update overlay tool name

---
+++ app/src/test/java/com/chasmet/remplissagepapierofficiel/PrecisionServerToolNamesTest.java
@@ -0,0 +1,4 @@
+package com.chasmet.remplissagepapierofficiel;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class PrecisionServerToolNamesTest { @Test public void paperUpdateOverlay(){assertEquals("paper_update_overlay",PrecisionServerToolNames.UPDATE_OVERLAY);} }
